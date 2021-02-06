eda1ee456197edf978ed942f998dee95f4d77b9e
Mike Stop Continues <mikestopcontinues@gmail.com>
2021-02-06 10:39:10 +0000

    Reduce proposals

--- src/proposals.js
+++ src/proposals.js
@@ -2,12 +2,6 @@
 
 module.exports = [
   require('@babel/plugin-proposal-export-default-from'),
-  require('@babel/plugin-proposal-logical-assignment-operators'),
-  require('@babel/plugin-proposal-nullish-coalescing-operator'),
-  require('@babel/plugin-proposal-optional-chaining'),
   require('@babel/plugin-proposal-partial-application'),
   require('@babel/plugin-proposal-throw-expressions'),
-  require('@babel/plugin-proposal-class-properties'),
-  require('@babel/plugin-proposal-private-methods'),
-  require('@babel/plugin-syntax-top-level-await'),
 ];
